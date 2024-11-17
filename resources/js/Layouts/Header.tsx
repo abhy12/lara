f839b415bb53ebadca3531de07a68d9c031375eb
abhy12 <98876719+abhy12@users.noreply.github.com>
2024-11-18 03:26:21 +0530

    add home link

--- resources/js/Layouts/Header.tsx
+++ resources/js/Layouts/Header.tsx
@@ -12,27 +12,28 @@ export default function Header() {
 
    return (
       <>
-      <header
-         className="text-white bg-secondary px-[5%] py-2 md:py-5 flex flex-wrap justify-between items-center gap-x-5 md:gap-x-12">
-         <div className='flex flex-wrap items-center gap-4 md:gap-6'>
-            <a className='grow max-w-20' href='https://indialeadersforsocialsector.com/' target='_blank'>
-               <img className='w-full' src='/assets/img/logo-1.svg' />
-            </a>
-            <a className='grow max-w-24' href='https://www.koitafoundation.org/' target='_blank'>
-               <img className='w-full brightness-0 invert' src='/assets/img/logo-2.png' />
-            </a>
-         </div>
-         <nav className="font-semibold md:text-lg hidden md:flex flex-col md:flex-row gap-x-5 md:gap-x-12 ml-auto">
-            <Link href={route('tools.index')}>Tools</Link>
-            <Link href={route('services.index')}>Service Provider</Link>
-            <Link href={route('faqs')}>FAQs</Link>
-            <Link href={route('about')}>About</Link>
-         </nav>
-         <button
-            className="text-tertiary bg-primary font-semibold px-3 py-1 rounded-lg"
-            onClick={() => setIsHelpModalActive( true )}
-         >Help</button>
-      </header>
+         <header
+            className="text-white bg-secondary px-[5%] py-2 md:py-5 flex flex-wrap justify-between items-center gap-x-5 md:gap-x-12">
+            <div className='flex flex-wrap items-center gap-4 md:gap-6'>
+               <a className='grow max-w-20' href='https://indialeadersforsocialsector.com/' target='_blank'>
+                  <img className='w-full' src='/assets/img/logo-1.svg' />
+               </a>
+               <a className='grow max-w-24' href='https://www.koitafoundation.org/' target='_blank'>
+                  <img className='w-full brightness-0 invert' src='/assets/img/logo-2.png' />
+               </a>
+            </div>
+            <nav className="font-semibold md:text-lg hidden md:flex flex-col md:flex-row gap-x-5 md:gap-x-12 ml-auto">
+               <Link href={route('home')}>Home</Link>
+               <Link href={route('tools.index')}>Tools</Link>
+               <Link href={route('services.index')}>Service Provider</Link>
+               <Link href={route('faqs')}>FAQs</Link>
+               <Link href={route('about')}>About</Link>
+            </nav>
+            <button
+               className="text-tertiary bg-primary font-semibold px-3 py-1 rounded-lg"
+               onClick={() => setIsHelpModalActive(true)}
+            >Help</button>
+         </header>
          <PopupModal
             isActive={isHelpModalActive}
             onClose={handleCloseModal}
