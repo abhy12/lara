b612b267acbd328b85850672cdd50d1142a94585
abhy12 <98876719+abhy12@users.noreply.github.com>
2024-11-18 22:00:34 +0530

    minor UI fixes

--- resources/js/Pages/Tools/Index.tsx
+++ resources/js/Pages/Tools/Index.tsx
@@ -11,37 +11,41 @@ interface Props {
 }
 
 export default function Index({ tools, categories }: Props) {
-   const [filterCatgory, setFilterCategory] = useState<number | null>( null );
+   const [filterCatgory, setFilterCategory] = useState( '' );
 
    return (
       <Layout>
          <Head title='Tools' />
-         <section className="text-white bg-tertiary px-[5%] pt-10 md:pt-20 pb-5 md:pb-7">
+         <section className="text-white bg-tertiary px-[5%] pt-10 md:pt-14 pb-6 md:pb-7">
             <nav className="text-[#909090] text-lg">
-               <Link href={route('home')}>Home</Link> &nbsp; / &nbsp;
+               <Link href={route('home')}>Home</Link>
+               &nbsp;&nbsp;/&nbsp;&nbsp;
                <Link href={route('tools.index')} className="text-secondary font-semibold">Tools</Link>
             </nav>
-            <div className="flex flex-wrap justify-between items-center">
-               <h1 className="font-DMSerifDisplay text-[3.625rem] md:text-[4.071rem]">Tools</h1>
-               <img src="/assets/img/wheel.gif" alt="Image" className="w-24" />
+            <div className="flex flex-wrap justify-between items-center mt-8 md:mt-6">
+               <h1 className="font-DMSerifDisplay text-5xl">Tools</h1>
+               <img src="/assets/img/wheel.gif" alt="Image" className="w-full max-w-16" />
             </div>
          </section>
 
-         <section className="px-[5%] py-10 md:pt-14 md:pb-28 flex flex-col md:flex-row gap-16 relative">
+         <section className="px-[5%] py-10 md:pt-14 md:pb-28 flex flex-col lg:flex-row gap-16 relative">
             <div className="basis-1/4">
-               <div className="bg-white max-w-80 p-5 shadow rounded-[15px]">
-                  <select name="tool"
-                     className="appearance-none text-secondary bg-white text-[1.375rem] font-medium w-full border-b-2 border-tertiary mb-5">
-                     <option value="">All Tools</option>
-                  </select>
+               <div className="bg-white lg:max-w-80 p-5 shadow rounded-[15px] text-lg font-medium">
+                  <button
+                     className="text-left text-secondary bg-white w-full border-b-2 border-tertiary mb-5"
+                     onClick={() => setFilterCategory('')}
+                  >All Tools
+                  </button>
                   <select
+                     className="text-secondary bg-white w-full border-b-2 border-tertiary
+                     appearance-none bg-dropdown bg-no-repeat bg-right px-2"
                      name="category"
-                     className="text-secondary bg-white text-[1.375rem] font-medium w-full border-b-2 border-tertiary appearance-none bg-dropdown bg-no-repeat bg-right"
+                     value={filterCatgory}
                      onChange={(e) => {
                         if( e.currentTarget.value !== '' ) {
-                           setFilterCategory( +e.currentTarget.value );
+                           setFilterCategory( e.currentTarget.value );
                         } else {
-                           setFilterCategory( null );
+                           setFilterCategory('');
                         }
                      }}
                   >
@@ -51,23 +55,26 @@ export default function Index({ tools, categories }: Props) {
                </div>
             </div>
             <div className="basis-3/4">
-               <div className="flex flex-wrap justify-around gap-y-14 md:gap-y-20">
+               <div className="grid grid-cols-2 md:grid-cols-3 gap-8 md:gap-8 gap-y-14 lg:gap-x-40 lg:gap-y-32">
                   {Array.isArray(tools) && tools.filter(tool => {
-                     if( filterCatgory === null ) return true
+                     if( filterCatgory === '' ) return true
 
                      if( !Array.isArray( tool.categories ) ) return false
 
                      for(let i = 0; i < tool.categories.length; i++ ) {
-                        if( tool.categories[i].id === filterCatgory || tool.categories[i].parent_id ) {
+                        //@ts-ignore
+                        if( tool.categories[i].id == filterCatgory || tool.categories[i].parent_id == filterCatgory ) {
                            return true;
                         }
                      }
                   }).map(tool =>
-                     <div className="basis-1/2 md:basis-1/3" key={tool.id}>
-                        <div className="max-w-max mx-auto md:mr-0 text-center">
+                     <div className="" key={tool.id}>
+                        <div className="text-center">
                            <div className="relative mb-5 md:mb-10">
                               <div
-                                 className="mx-auto bg-white w-36 md:w-60 aspect-square flex justify-center items-center border border-tertiary rounded-full p-4 md:p-7">
+                                 className="bg-white w-full aspect-square
+                                 flex justify-center items-center border border-tertiary rounded-full p-4 md:p-7"
+                              >
                                  <img
                                     src="/assets/img/mform.png"
                                     alt="Image"
@@ -75,7 +82,8 @@ export default function Index({ tools, categories }: Props) {
                               </div>
                               <Link
                                  href={route('tools.show', { id: tool.id })}
-                                 className="bg-tertiary w-11 md:w-20 aspect-square p-2 rounded-full flex justify-center items-center absolute right-0 bottom-0"
+                                 className="bg-tertiary w-full max-w-10 md:max-w-14 aspect-square p-2
+                                 rounded-full flex justify-center items-center absolute right-0 bottom-0"
                               >
                                  <img
                                     src="/assets/img/arrow.svg"
@@ -83,7 +91,7 @@ export default function Index({ tools, categories }: Props) {
                                  />
                               </Link>
                            </div>
-                           <h2 className="text-tertiary font-semibold text-2xl md:text-[2.188rem] leading-[1.457em]">{tool.name}</h2>
+                           <h2 className="text-tertiary font-semibold text-xl md:text-2xl">{tool.name}</h2>
                            <p className="text-secondary font-medium md:text-[1.375rem] leading-[1.455em]">Dhwani RIS</p>
                         </div>
                      </div>
