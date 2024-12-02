1371dfba44d7c23ba0b583a0bb80f33e223d8fbd
abhy12 <98876719+abhy12@users.noreply.github.com>
2024-12-02 18:44:20 +0530

    add link sharing logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta property="og:image" itemprop="image" content="/assets/img/logo-2.png">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Scripts -->
